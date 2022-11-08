96ac06c6eb1542981d4c5c521390d1bbe2717ff8
Dung Nguyen <johnny@MacBook-Pro.local>
2022-11-08 23:10:46 +1000

    add touch bar color

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import styled from 'styled-components';
+
 import { FifthRow } from './Components/FifthLine';
 import { FirstRow } from './Components/FirstLine';
 import { ForthRow } from './Components/ForthLine';
@@ -7,13 +8,29 @@ import { SecondRow } from './Components/SecondLine';
 import { ThirdRow } from './Components/ThirdLine';
 
 function App() {
+  const initColor = "linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%)";
+  const deepPurple = "linear-gradient(90deg, rgba(79,58,85,1) 0%, rgba(33,10,50,1) 44%, rgba(136,25,189,1) 100%)";
+  const greenApline = "linear-gradient(90deg, rgba(68,110,71,1) 0%, rgba(10,50,30,1) 44%, rgba(8,120,22,1) 100%)";
+  const midNight = "linear-gradient(90deg, rgba(115,115,115,1) 0%, rgba(13,15,13,1) 44%, rgba(12,26,28,1) 100%);";
+  const keyboard = "Keyboard"
+
+  const [color, setColor] = useState(initColor);
+  console.log(color)
+
+
   return (
     <Page>
       <h1>Welcome to Keyboard Pro</h1>
       <Keyboard>
-        <Title>MAC Keyboard</Title>
+        <Title>MAC {keyboard}</Title>
+        <Select value={color} onChange={e => setColor(e?.target.value)}>
+          <option value="">-- Select color -- </option>
+          <option value={deepPurple}>Deep Purple</option>
+          <option value={greenApline}>Green Alpine</option>
+          <option value={midNight}>Midnight Black</option>
+        </Select>
         <Layout>
-          <TouchBar></TouchBar>
+          <TouchBar color={color}></TouchBar>
           <FirstRow />
           <SecondRow></SecondRow>
           <ThirdRow></ThirdRow>
@@ -63,14 +80,25 @@ const Layout = styled.div`
   padding: 8px 8px 2px 8px;
   border-radius: 12px;
   background: #636060;
-
 `;
 
 const TouchBar = styled.div`
-  background: red;
-  height: 12%;
   background: linear-gradient(90deg, rgba(131,58,180,1) 0%, rgba(253,29,29,1) 50%, rgba(252,176,69,1) 100%);
+  height: 12%;
+  background: ${(props) => props.color};
   border-radius: 12px;
   margin: 0 0 8px 0;
+`;
+
+const Select = styled.select`
+  padding: 10px;
+  background:#636060;
+  border:none;
+  font-size: 1.2rem;
+  margin-bottom: 20px;
+`;
+
+const SpanRandomColor = styled.span`
+  color: ${(props) => props.aColor};
 `;
 
